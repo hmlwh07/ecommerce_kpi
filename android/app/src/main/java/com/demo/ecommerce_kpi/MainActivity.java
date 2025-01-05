581097ceaed3e9afd6c07848edea8f0e6dc107d8
hayman <hayman@blue-stone.net>
2025-01-05 22:02:52 +0630

    Initial commit

---
+++ android/app/src/main/java/com/demo/ecommerce_kpi/MainActivity.java
@@ -0,0 +1,6 @@
+package com.demo.ecommerce_kpi;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Scaffold(backgroundColor: Colors.tealAccent,appBar: AppBar(title: Text('Loey Book Shop'),backgroundColor: Colors.teal,),),),);
+
